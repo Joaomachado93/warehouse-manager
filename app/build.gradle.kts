8c8f42d00850c5cdb5ee2abdf98518c3199c119a
Joaomachado93 <joaomachadoporto@hotmail.com>
2026-03-27 21:56:11 +0000

    Add guava dependency for CameraX ListenableFuture

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,6 +102,7 @@ dependencies {
     implementation("androidx.camera:camera-camera2:1.3.4")
     implementation("androidx.camera:camera-lifecycle:1.3.4")
     implementation("androidx.camera:camera-view:1.3.4")
+    implementation("com.google.guava:guava:32.1.3-android")
 
     // ML Kit Barcode Scanning
     implementation("com.google.mlkit:barcode-scanning:17.2.0")
